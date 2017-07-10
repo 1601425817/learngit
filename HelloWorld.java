fe5687dcfdca2f70a9fdd76a46f9dbb9a0e18ee1
1601425817 <2642822066@qq.com>
2017-07-10 23:46:22 +0800

    添加了一个HelloWorld文件

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello");
+		}
+	}
+
+	public void say()
+	{
+		//stupid
+	}
+
+}
